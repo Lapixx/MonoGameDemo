41cd373a277c0054ca7b41c7219841dd98df6e36
Tijn Kersjes <tijn@divbyzero.nl>
2016-09-19 16:04:55 +0200

    Initial commit

---
+++ MonoApp/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoApp
+{
+    public class MyGame : Game
+    {
+
+        GraphicsDeviceManager graphics;
+
+        public MyGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Console.WriteLine("init");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back ==
+                ButtonState.Pressed || Keyboard.GetState().IsKeyDown(
+                    Keys.Escape))
+                Exit();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGame myGame = new MyGame();
+            myGame.Run();
+        }
+    }
+}
+
+
